f66cdbb50508d27bad92d2cc855c9cbc1051f183
PPTaa <chobo05@naver.com>
2020-01-13 16:41:36 +0900

    js function 학습

---
+++ syntax/function.js
@@ -0,0 +1,23 @@
+function f123(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+}
+
+f123();
+console.log("a");
+console.log("b");
+console.log("c");
+f123();
+console.log("d");
+console.log("e");
+console.log("f");
+console.log("g");
+f123();
+console.log("h");
+console.log("i");
+console.log("j");
+console.log("k");
+console.log("l");
+f123();
---
+++ syntax/function2.js
@@ -0,0 +1,11 @@
+ex_round = Math.round(3.14)
+console.log(ex_round)
+ex_round2 = Math.round(3.64)
+console.log(ex_round2)
+
+function sum(frist, second) { //매개체들 parameter(매개변수)
+
+    return frist + second;
+}
+
+console.log(sum(2, 4)); // 입력값들을 argument (인자)
