94be28bcca540d5c62a115ecc4109ce705fc62c3
PPTaa <chobo05@naver.com>
2020-01-15 17:15:44 +0900

    delete 구현

--- main.js
+++ main.js
@@ -68,7 +68,12 @@ var app = http.createServer(function (request, response) {
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     var title = queryData.id;
                     var template = templateHTML(title, list, `<h1>${title}</h1>${description}`, googleanaltyics, tawk, disqus,
-                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                        `<a href="/create">create</a> 
+                        <a href="/update?id=${title}">update</a> 
+                        <form action="delete_process" method="post">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                        </form>`);
                     response.writeHead(200);
                     response.end(template);
                 });
@@ -147,6 +152,19 @@ var app = http.createServer(function (request, response) {
                     response.end();
                 });
             });
+        });
+    }else if(pathname==="/delete_process"){
+        var body = '';
+        request.on('data', function (data) {
+            body = body + data;
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, function(error){
+               response.writeHead(302, {Location: `/`});
+               response.end();
+            })
         });
     }else {
         response.writeHead(404);
