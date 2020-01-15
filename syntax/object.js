9ffa803d03fa5c9e40008024f94685228a8cb585
PPTaa <chobo05@naver.com>
2020-01-15 17:23:09 +0900

    객체의 형식 간단학습

---
+++ syntax/object.js
@@ -0,0 +1,10 @@
+var members = ['apple', 'bread', 'cat', 'dog'];
+console.log(members[1]); //bread
+
+var roles = {
+    'programmer':'apple',
+    'designer':'bread',
+    'manager':'cat',
+    'CEO':'dog'
+}
+console.log(roles.designer);
